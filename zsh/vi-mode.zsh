a52b3450c1c527c49e03b0777efb0b6525eae75b
George Sudarkoff <george@sudarkoff.com>
2014-01-13 21:19:45 -0800

    Add my custom stuff to path. Add pick-vi() to zsh/vi-mode.zsh.

--- zsh/vi-mode.zsh
+++ zsh/vi-mode.zsh
@@ -1,3 +1,19 @@
 set -o vi
-export EDITOR=${VI:-vim}
-export VISUAL=${VI:-vim}
+
+### Picks a platform-appropriate vi
+function pick-vi () {
+    if [ -f "$HOME/bin/$OSTYPE/mvim" ]; then
+        echo "$HOME/bin/$OSTYPE/mvim"
+    elif [ -f "/usr/bin/vim" ]; then
+        echo "/usr/bin/vim"
+    elif [ -f "/usr/bin/vi" ]; then
+        echo "/usr/bin/vi"
+    elif [ -f "/bin/vi" ]; then
+        echo "/bin/vi"
+    fi
+}
+
+export VI="$(pick-vi)"
+export EDITOR="${VI:-vim}"
+export VISUAL="${VI:-vim} -f"
+
