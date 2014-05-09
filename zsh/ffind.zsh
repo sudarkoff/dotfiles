a66d14fcecb24252ebb120f86895929c77455808
George Sudarkoff <george@sudarkoff.com>
2014-05-08 20:21:06 -0700

    Add xml and json to *find family of aliases. Reimplement a few things to streamline the usage: renamed _find_regex to findgrep; allow to pass a wildcard instead of a language to findgrep. E.g.: findgrep *.md word.

--- zsh/ffind.zsh
+++ zsh/ffind.zsh
@@ -19,7 +19,7 @@ function _relpath () {
    echo ${back}${target#$common_part}
 }
 
-# Enumerate file extensions for various programming languages
+# Enumerate file extensions for various programming and scripting languages and text document types
 c_ext=("*.c" "*.cc" "*.cp" "*.cxx" "*.cpp" "*.c++" "*.C" "*.i" "*.ii" "*.h" "*.hh" "*.hp" "*.hxx" "*.hpp" "*.h++")
 py_ext=("*.py")
 pl_ext=("*.pl" "*.pm")
@@ -27,42 +27,55 @@ java_ext=("*.java")
 js_ext=("*.js")
 sql_ext=("*.sql")
 html_ext=("*.htm" "*.html" "*.css")
+xml_ext=("*.xml" "*.xsl")
+json_ext=("*.json")
 sc_ext=("SConstruct" "SConscript" "*.sc")
 mk_ext=("Makefile" "*.mk")
 src_ext=(${c_ext} ${py_ext} ${pl_ext} ${java_ext} ${js_ext} ${sql_ext} ${html_ext} ${sc_ext} ${mk_ext})
+sh_ext=("*.sh" "*.zsh")
 md_ext=("*.md" "*.mdown" "*.markdown")
 
 # Turn the list of extensions into a condition string for 'find' utility
-function _lang_extensions () {
-    local lng_exts=${1}"_ext"
-    local exts=""
-    for e in ${(P)lng_exts}; do
-        if [ -z "${exts}" ]; then
-            exts="-name \""$e"\""
-        else
-            exts="$exts -o -name \""$e"\""
-        fi
+function _lang_extensions_for_find () {
+  local exts=$1"_ext"
+  local find_expr=""
+
+  if [[ -z "${(P)exts}" ]]; then
+    # looks like the first parameter is not the list of file extensions
+    # perhaps a wildcard?
+    find_expr="-name \""$1"\""
+  else
+    for e in ${(P)exts}; do
+      if [ -z "${find_expr}" ]; then
+          find_expr="-name \""$e"\""
+      else
+          find_expr="$find_expr -o -name \""$e"\""
+      fi
     done
-    echo "$exts"
+  fi
+  echo "$find_expr"
 }
 
-# Locations to ignore when searching for symbols
-prune_dir="-type d -name .git -prune -o -type d -name .svn -prune -o -type d -name CVS -prune"
+# Grep for a pattern in all source files for a given language (e.g.: findgrep c <regex>)
+function findgrep () {
+  if [[ "$1" == "--help" || "$1" == "" ]]; then
+    echo "Usage: findgrep <pattern> <c|py|pl|java|js|sql|html|xml|json|sc|mk|src|sh|md|...>"
+    echo "Search for a 'regexp' in all files of a specified type starting from the current directory."
+    return 1
+  fi
 
-# Grep for a pattern in all all source files for a given language (e.g.: _find_regex c <regex>)
-function _find_regex () {
-   if [[ "$1" == "--help" || "$1" == "" ]]; then
-      echo "Usage: _find_regex <c||py|pl|java|js|sql|html|sc|mk|src|md> <regexp>"
-      echo "Search for a 'regexp' in all source files of a specified language starting from the current directory."
-      return 1
-   fi
+  local ext=$1
+  local pattern=$2
 
-   eval find . ${prune_dir} -o -type f \\\( $(_lang_extensions $1) \\\) -print0 | xargs -0 grep --color --line-number --extended-regexp $2
+  # Locations to ignore when searching for symbols
+  local prune_dir="-type d -name .git -prune -o -type d -name .svn -prune -o -type d -name CVS -prune"
+
+  eval find . ${prune_dir} -o -type f \\\( $(_lang_extensions_for_find ${ext}) \\\) -print0 | xargs -0 grep --color --line-number --extended-regexp ${pattern}
 }
 
-# Define Xfind aliases for various programming languages
-for lng in c py pl java js sql html sc mk src md; do
-   # Search recursively starting from the current directory (local)
-   alias ${lng}find='_find_regex '$lng' $1'
+# Define *find aliases for various programming languages
+for lng in c py pl java js sql html xml json sc mk src sh md; do
+  # Search recursively starting from the current directory (local)
+  alias ${lng}find='findgrep '$lng' $1'
 done
 
