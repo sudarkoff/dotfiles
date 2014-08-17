05e72e7cb9d29f448d666c69ec55926086c91e1f
George Sudarkoff <george@sudarkoff.com>
2014-08-16 19:39:15 -0700

    Update install.sh
    Add installing prerequisites (no checking yet): homebrew, git

--- install.sh
+++ install.sh
@@ -2,8 +2,15 @@
 
 if [ ! -d "$HOME/.yadr" ]; then
     echo "Installing YADR for the first time"
+
+    # TODO: Check and install prerequisites
+    ruby -e "$(curl -fsSL https://raw.github.com/Homebrew/homebrew/go/install)"
+    brew update
+    brew install git
+
     git clone https://github.com/sudarkoff/dotfiles.git "$HOME/.yadr"
     cd "$HOME/.yadr"
+
     [ "$1" == "ask" ] && export ASK="true"
     rake install
 else
